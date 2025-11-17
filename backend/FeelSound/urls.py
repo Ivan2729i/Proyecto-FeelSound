e85d32cc0df173aa000cae19bd1780898d3f4b35
Ivan2729i <ivanpazvalladares@gmail.com>
2025-11-16 22:28:00 -0600

    Arreglo de UI UX dashboard7

--- backend/FeelSound/urls.py
+++ backend/FeelSound/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from django.views.static import serve
 
 urlpatterns = [
     path("", RedirectView.as_view(pattern_name="accounts:login", permanent=False), name="home"),
@@ -12,6 +13,12 @@ urlpatterns = [
     path("", include("home.urls")),
     path("api/", include("api.urls")),
     path("api/v1/", include("accounts.api_urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
 
 
